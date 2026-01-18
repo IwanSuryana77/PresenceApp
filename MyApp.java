ee46fa784e2d6a7200bbedd950eaa90dffc76bfd
Lenovo <iwansuryana025@gmail.com>
2026-01-18 22:35:36 +0700

    Update

--- MyApp.java
+++ MyApp.java
@@ -9,3 +9,15 @@ public class MyApp extends Application {
         MediaManager.init(this, config);
     }
 }
+
+// public class MyApp extends Application {
+//     @Override
+//     public void onCreate() {
+//         super.onCreate();
+
+//         Map<String, Object> config = new HashMap<>();
+//         config.put("cloud_name", "facesign123"); // ganti dengan punyamu
+
+//         MediaManager.init(this, config);
+//     }
+// }
